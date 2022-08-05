d95db64e869fcefa7741e1f703f8c11509887c3e
BakayAlexander <bakay.dvr@gmail.com>
2022-08-05 21:22:52 +0300

    feat: implement HoatToast

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import React from 'react';
 import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import { Toaster } from 'react-hot-toast';
+import useHoatToast from '../utils/useHoatToast';
 
 // interface Session {
 //   user?: {
@@ -36,18 +38,18 @@ const Sidebar: React.FC = () => {
         </div>
         <div className="hidden md:inline text-left dark:text-white/70 text-sm">
           <div className="font-medium space-y-0.5 dark:border-t border-gray-600 py-3 px-4 ">
-            <div className="sidebar__button">
+            <div className="sidebar__button" onClick={useHoatToast}>
               <h4>Who viewed your profile</h4>
               <span className="text-blue-500">303</span>
             </div>
-            <div className="sidebar__button">
+            <div className="sidebar__button" onClick={useHoatToast}>
               <h4>Views of your post</h4>
               <span className="text-blue-500">1,002</span>
             </div>
           </div>
           <div className="font-medium space-y-0.5 dark:border-t border-gray-600 py-3 px-4 ">
             <h4 className="leading-4 text-xs">Access exclusive tools & insights</h4>
-            <div className="sidebar__button">
+            <div className="sidebar__button" onClick={useHoatToast}>
               <h4 className="dark:text-white font-medium">
                 <span className="w-3 h-3 bg-gradient-to-tr from-yellow-700 to-yellow-200 inline-block rounded-sm mr-1" />{' '}
                 Try Premium for free
@@ -55,7 +57,7 @@ const Sidebar: React.FC = () => {
             </div>
           </div>
           <div className="font-medium space-y-0.5 dark:border-t border-gray-600 py-3 px-4 flex items-center space-x-1.5">
-            <div className="sidebar__button">
+            <div className="sidebar__button" onClick={useHoatToast}>
               <BookmarkOutlinedIcon className="!-ml-1" />
               <h4 className="dark:text-white font-medium">My items</h4>
             </div>
@@ -63,16 +65,25 @@ const Sidebar: React.FC = () => {
         </div>
       </div>
       <div className="hidden md:flex bg-white dark:bg-[#1d2226] text-black/60 dark:text-white/70 overflow-hidden flex-col space-y-2 pt-2.5 rounded-lg sticky top-2.5 border border-gray-300 dark:border-none">
-        <p className="sidebar__link">Groups</p>
+        <p className="sidebar__link" onClick={useHoatToast}>
+          Groups
+        </p>
         <div className="flex items-center justify-between">
-          <p className="sidebar__link">Events</p>
+          <p className="sidebar__link" onClick={useHoatToast}>
+            Events
+          </p>
           <AddRoundedIcon className="!h-5" />
         </div>
-        <p className="sidebar__link">Followed Hashtags</p>
+        <p className="sidebar__link" onClick={useHoatToast}>
+          Followed Hashtags
+        </p>
         <div className="font-medium space-y-0.5 dark:border-t border-gray-600 py-3 px-4 space-x-1.5 text-center cursor-pointer opacity-80 hover:opacity-100">
-          <h4 className="dark:text-white font-medium text-sm">Discover More</h4>
+          <h4 className="dark:text-white font-medium text-sm" onClick={useHoatToast}>
+            Discover More
+          </h4>
         </div>
       </div>
+      <Toaster position="bottom-center" />
     </div>
   );
 };
